75a4607e33da916530a3f0d451b699868135f14c
Berk Özbalcı <berkozbalci@gmail.com>
2018-11-15 22:54:26 +0300

    Further optimize imports

--- src/main/java/com/twentythree/space/repository/MatchRepository.java
+++ src/main/java/com/twentythree/space/repository/MatchRepository.java
@@ -5,7 +5,6 @@ import com.twentythree.space.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
