7cf82a4d14f78f8bd3cb7ce9faf01b32b8ccda32
Berk Özbalcı <berkozbalci@gmail.com>
2018-11-15 22:49:17 +0300

    Add leaderboard functionality

--- src/main/java/com/twentythree/space/controller/LeaderboardController.java
+++ src/main/java/com/twentythree/space/controller/LeaderboardController.java
@@ -1,6 +1,8 @@
 package com.twentythree.space.controller;
 
 import com.twentythree.space.entity.Player;
+import com.twentythree.space.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,4 +13,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/leaderboard")
 public class LeaderboardController {
+    @Autowired
+    private MatchRepository matchRepository;
+
+    @GetMapping("/weekly")
+    List<Object[]> getSevenDaysLeaderboard() {
+        return matchRepository.getSevenDaysLeaderboard(10);
+    }
+
+    @GetMapping("/all")
+    List<Object[]> getAllTimeLeaderboard() {
+        return matchRepository.getSevenDaysLeaderboard(10);
+    }
 }
--- src/main/java/com/twentythree/space/repository/MatchRepository.java
+++ src/main/java/com/twentythree/space/repository/MatchRepository.java
@@ -3,11 +3,34 @@ package com.twentythree.space.repository;
 import com.twentythree.space.entity.Match;
 import com.twentythree.space.entity.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.util.Pair;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MatchRepository extends JpaRepository<Match, Long> {
     Optional<Match> findByMatchIdAndPlayer(long matchId, Player player);
+
+    @Query(value = "SELECT p.name, SUM(gm.score) as total_score\n" +
+            "FROM game_match gm\n" +
+            "INNER JOIN player p\n" +
+            "ON gm.player_id = p.id\n" +
+            "GROUP BY p.id\n" +
+            "ORDER BY total_score DESC\n" +
+            "LIMIT :limit", nativeQuery = true)
+    List<Object[]> getAllTimeLeaderboard(@Param("limit") long limit);
+
+    @Query(value = "SELECT p.name, SUM(gm.score) as total_score\n" +
+            "FROM game_match gm\n" +
+            "INNER JOIN player p\n" +
+            "ON gm.player_id = p.id\n" +
+            "WHERE gm.created_at BETWEEN DATE_SUB(NOW(), INTERVAL 7 DAY) AND NOW()\n" +
+            "GROUP BY p.id\n" +
+            "ORDER BY total_score DESC\n" +
+            "LIMIT :limit", nativeQuery = true)
+    List<Object[]> getSevenDaysLeaderboard(@Param("limit") long limit);
 }
