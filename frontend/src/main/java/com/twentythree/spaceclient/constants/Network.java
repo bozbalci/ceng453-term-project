ef6939d58a1bf4fb452a590234343f2b0b54762d
Berk Özbalcı <berkozbalci@gmail.com>
2019-01-17 03:22:37 +0300

    client jar

--- frontend/src/main/java/com/twentythree/spaceclient/constants/Network.java
+++ frontend/src/main/java/com/twentythree/spaceclient/constants/Network.java
@@ -1,9 +1,9 @@
 package com.twentythree.spaceclient.constants;
 
 public class Network {
-    public static final String API_URL = "http://localhost:8080";
-    public static final String SERVER_HOST = "localhost";
-    public static final Integer SERVER_PORT = 7777;
+    public static final String API_URL = "http://144.122.71.144:8080";
+    public static final String SERVER_HOST = "144.122.71.144";
+    public static final Integer SERVER_PORT = 8082;
 
     public static final Integer TICK_RATE = 120;
 
