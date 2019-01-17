289d874e101da01e089f38eecf4da4010444daf7
Berk Özbalcı <berkozbalci@gmail.com>
2019-01-17 03:15:19 +0300

    server jar

--- server/src/main/java/com/twentythree/server/constant/Network.java
+++ server/src/main/java/com/twentythree/server/constant/Network.java
@@ -1,7 +1,7 @@
 package com.twentythree.server.constant;
 
 public class Network {
-    public static final int PORT = 7777;
+    public static final int PORT = 8082;
 
     public static final Long CMD_START = 0L;
     public static final Long CMD_POSITION = 1L;
